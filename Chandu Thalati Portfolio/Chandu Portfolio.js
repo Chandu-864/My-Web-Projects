9e9c2c27b487bfdc641f669009cf2d1d0a33ce5a
Chandu Thalati <141419083+Chandu-864@users.noreply.github.com>
2024-09-20 13:34:07 -0400

    HTML

---
+++ Chandu Thalati Portfolio/Chandu Portfolio.js
@@ -0,0 +1,23 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tablecontents = document.getElementsByClassName("table-contents");
+
+function opentab(tabname){
+    for(x of tablinks){
+        x.classList.remove("active-link");
+    }
+    for(y of tablecontents){
+        y.classList.remove("active-table");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-table");
+}
+
+function openMenu() {
+    console.log("open menu clicked");
+    document.getElementById("sidemenu").style.display = "block";
+}
+
+function closeMenu() {
+    console.log("close menu clicked");
+    document.getElementById("sidemenu").style.display = "none";
+}
